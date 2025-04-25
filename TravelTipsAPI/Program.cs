7cb7b73b7952c22ec002008156e88c546c09b185
unknown <kaiwen.chu58@gmail.com>
2025-04-25 15:41:49 -0700

    match to main

--- TravelTipsAPI/Program.cs
+++ TravelTipsAPI/Program.cs
@@ -9,29 +9,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddDbContext<TravelTipsContext>(options =>
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("TravelTips"))
-    options.UseSqlServer(builder.Configuration.GetConnectionString("TravelTipsLocal"))
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TravelTips"))
+//options.UseSqlServer(builder.Configuration.GetConnectionString("TravelTipsLocal"))
 );
 
 // Add authentication to the container.
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/"; // Must match "iss"
-    options.Audience = builder.Configuration["Auth0:Audience"]; // Must match "aud"
-    options.TokenValidationParameters = new TokenValidationParameters
+builder
+    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuerSigningKey = true,
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true
-    };
-});
+        options.Authority = $"https://{builder.Configuration["Auth0:Domain"]}/"; // Must match "iss"
+        options.Audience = builder.Configuration["Auth0:Audience"]; // Must match "aud"
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+        };
+    });
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
